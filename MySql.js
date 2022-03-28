6b113ec8f494475450a4be7e4ef57bf4fb314d9e
masdefry <muhammaddefryan@gmail.com>
2022-03-28 11:00:31 +0700

    SQL CRUD

---
+++ MySql.js
@@ -0,0 +1,22 @@
+// Create Database : create database nama_database;
+// Show Database : show databases;
+// Select Database : use nama_database;
+// Create Table : 
+            // create table users (
+            // id int(5) auto_increment primary key,
+            // username varchar(30) not null,
+            // email varchar(50) not null,
+            // password varchar(255) not null
+            // );
+
+// Insert Data : 
+            // insert into users (username, email, password) values ("ryandefryan", "ryan@gmail.com", "123123123");
+
+// Read Data : 
+            // select * from nama_table;
+
+// Update Data :
+            // update nama_table set username="mdefryan" where id=1;
+
+// Delete Data : 
+            //  delete from nama_table where id=1;
